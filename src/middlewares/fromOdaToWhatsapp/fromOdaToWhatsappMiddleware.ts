fa0c5084cf9d43710f4d81139a1b63aeb7e8e87d
leosfs <leonardo.silva@escolamobile.com.br>
2023-07-09 20:37:59 -0300

    fix: debug

--- src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
+++ src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
@@ -27,66 +27,91 @@ export function handleBotResponse(
     })
     .on(
       WebhookEvent.MESSAGE_RECEIVED,
-      (receivedMessage: { number: any; messagePayload: { text: any } }) => {
+      (receivedMessage: { number: any; messagePayload: {
+        actions: any; text: any 
+} }) => {
         console.log(
           "Received a message from ODA, processing message before sending to WhatsApp."
         );
         // console.log("------------------------------>>>>")
-        console.log(JSON.stringify(receivedMessage))
+        // console.log(JSON.stringify(receivedMessage))
+
+        const contentMessage:any = {
+          messaging_product: "whatsapp",
+          to: from,
+        };
+        
+        const quikReplyInteractive:any = {
+          type: "button",
+          header: {
+            type: "text",
+            text: "", 
+          },
+          body: {
+            text: receivedMessage.messagePayload.text,
+          },
+          footer: {
+            text: "", 
+          },
+        };
+        
+        const actionsQuikReply = receivedMessage.messagePayload.actions;
+        
+        if (actionsQuikReply.length === 0) {
+          contentMessage.text = { body: receivedMessage.messagePayload.text };
+        }
+        
+        if (actionsQuikReply.length > 0 && actionsQuikReply.length <= 3) {
+          contentMessage.type = "interactive";
+          quikReplyInteractive.type = "button"
+
+          quikReplyInteractive.action = { buttons: [] };
+        
+          actionsQuikReply.forEach((content:any) => {
+            const button:any = {
+              "type": "reply",
+              "reply": {
+                "id": content.label,
+                "title": content.label,
+              },
+            };
+        
+            quikReplyInteractive.action.buttons.push(button);
+          });
+        }
+        if (actionsQuikReply.length > 3) {
+          contentMessage.type = "interactive";
+          quikReplyInteractive.type = "list"
+
+          quikReplyInteractive.action = { button: "cta-button-content" };
+          quikReplyInteractive.action = { sections: [
+            {
+              "title": receivedMessage.messagePayload.text,
+              "rows": [ ]
+            }
+          ] };
+        
+          actionsQuikReply.forEach((content:any) => {
+            const button:any = {
+                "id": content.label,
+                "title": content.label,
+            };
+        
+            quikReplyInteractive.action.sections.push(button);
+          });
+        }
+
         const token = env.whatsappToken;
 
-        // axios.post(
-        //   `https://graph.facebook.com/v16.0/${phon_no_id}/messages`,
-        //   {
-        //     messaging_product: "whatsapp",
-        //     to: from,
-        //     text: {
-        //       body: receivedMessage.messagePayload.text,
-        //     },
-        //   },
-        //   {
-        //     headers: {
-        //       Authorization: `Bearer ${token}`,
-        //       "Content-Type": "application/json",
-        //     },
-        //   }
-        // );
-        try {
-          const payload = {
-            messaging_product: 'whatsapp',
-            to: from,
-            text: {
-              body: receivedMessage.messagePayload.text,
+        axios.post(
+          `https://graph.facebook.com/v16.0/${phon_no_id}/messages`, {...contentMessage, quikReplyInteractive},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
             },
-            buttons: [
-              {
-                type: 'postback',
-                title: 'Botão 1',
-                payload: 'payload1',
-              },
-              {
-                type: 'postback',
-                title: 'Botão 2',
-                payload: 'payload2',
-              },
-            ],
-          };
-      
-          const response = axios.post(
-            `https://graph.facebook.com/v16.0/${phon_no_id}/messages`,
-            payload,
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-              },
-            }
-          );
-      
-          // console.log(response.data);
-        } catch (error) {
-          console.error('Erro ao enviar a mensagem:', error);
-        }
+          }
+        );
       }
     );
 
