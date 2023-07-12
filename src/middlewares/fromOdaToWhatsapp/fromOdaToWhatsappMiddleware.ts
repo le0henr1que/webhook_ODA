1a09224a54889a89eebf696821b9705257cb05a5
leosfs <leonardo.silva@escolamobile.com.br>
2023-07-11 22:04:09 -0300

    feat: solving button reply

--- src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
+++ src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
@@ -93,70 +93,16 @@ export function handleBotResponse(req: Request, res: Response, next: NextFunctio
                 rows: [],
               },
             ];
+
             receivedMessage.messagePayload.actions.forEach((content: any) => {
               const button: any = {
-                type: "reply",
-                reply: {
-                  id: content.label,
-                  title: "Selecionar",
-                },
+                id: content.label,
+                title: " ",
+                description: content.label
               };
-            
-              interactive.action.buttons.push(button);
-            
-              const payloadSend: any = {
-                messaging_product: "whatsapp",
-                to: from,
-                type: "interactive",
-                interactive: {
-                  type: "button",
-                  header: {
-                    type: "text",
-                    text: "",
-                  },
-                  body: {
-                    text: content.label,
-                  },
-                  footer: {
-                    text: "",
-                  },
-                  action: {
-                    buttons: [button],
-                  },
-                },
-              };
-            
-       
-              axios
-              .post(
-                `https://graph.facebook.com/v16.0/${phon_no_id}/messages`,
-                payloadSend,
-                {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                  },
-                }
-              )
-              .then((response) => {
-                // Lógica para lidar com a resposta da solicitação POST
-                // console.log(response)
 
-              })
-              .catch((error) => {
-                // console.log(error)
-                // Lógica para lidar com erros na solicitação POST
-              });
+              interactive.action.sections[0].rows.push(button);
             });
-            // receivedMessage.messagePayload.actions.forEach((content: any) => {
-            //   const button: any = {
-            //     id: content.label,
-            //     title: " ",
-            //     description: content.label
-            //   };
-
-            //   interactive.action.sections[0].rows.push(button);
-            // });
             // console.log(interactive);
           }
         }
