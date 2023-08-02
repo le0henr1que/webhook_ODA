cbf00c8b6e4304e1c46bbb02e140dc54bbed8798
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-08-01 21:08:30 -0300

    fix: resolvendo problema de condition

--- src/modules/WhatsApp/useCase/fromWhatsappToOda/WebhookUseCase.ts
+++ src/modules/WhatsApp/useCase/fromWhatsappToOda/WebhookUseCase.ts
@@ -32,10 +32,10 @@ export class WebhookUseCase {
   
 
     if (payload.entry[0].changes[0].value.messages[0]?.interactive?.list_reply) {
-      // msg_body = payload.entry[0].changes[0].value.messages[0].interactive.list_reply.description;
-      msg_body = payload.entry[0].changes[0].value.messages[0].interactive.list_reply.id;
-      console.log(payload.entry[0].changes[0].value.messages[0].interactive.list_reply)
-      console.log(msg_body)
+      msg_body = payload.entry[0].changes[0].value.messages[0].interactive.list_reply.description;
+      // msg_body = payload.entry[0].changes[0].value.messages[0].interactive.list_reply.id;
+      // console.log(payload.entry[0].changes[0].value.messages[0].interactive.list_reply)
+      // console.log(msg_body)
     } else if (payload.entry[0].changes[0].value.messages[0]?.interactive?.button_reply) {
       // msg_body = payload.entry[0].changes[0].value.messages[0].interactive.button_reply.title;
       msg_body = payload.entry[0].changes[0].value.messages[0].interactive.button_reply.id;
