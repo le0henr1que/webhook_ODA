f9c709581374abd86ae2f063b5ec5633fa170df1
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-08-03 09:00:25 -0300

    fix: erro na aciton

--- src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
+++ src/middlewares/fromOdaToWhatsapp/fromOdaToWhatsappMiddleware.ts
@@ -226,9 +226,10 @@ export async function handleBotResponse(req: Request, res: Response, next: NextF
                 if(shortenedTitle.length >= 24){
                   shortenedTitle = content.label.split(" ")[0]
                 }
-                
+                let action = content.postback.action ? content.postback.action : content.label;
+
                 return {
-                  id: content.label,
+                  id: action,
                   title: shortenedTitle,
                   description: content.label
                 };
