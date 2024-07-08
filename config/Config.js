4d76b5f748098737042c81df0aaad0f5a62b0b10
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-07-08 11:11:23 -0300

    fix: new version

--- config/Config.js
+++ config/Config.js
@@ -6,16 +6,15 @@ exports.ODA_WEBHOOK_SECRET = "svMjNN9DoW0bc1kAuiBvJfx2nBJeiBhy";
 // WhatsApp Details
 exports.API_URL = "https://graph.facebook.com";
 exports.ENDPOINT_API = "messages";
-exports.VERIFY_TOKEN = process.env.VERIFY_TOKEN || "access-token";
+exports.VERIFY_TOKEN = "access-token";
 exports.ACCESS_TOKEN =
-  process.env.ACCESS_TOKEN ||
   "EAADaO3ZCOVKoBO5xLGme3Qkey8kyecWYqOaWTjynDTk4mcYhcOL9uxGWg1VCZB9gxhYqtSTR08alOd3RREBq16PWJMcTsHOlHCCpV7NAA9rqqZCetjAoRsyqMBbZA9t7ZBLc3CsFK38fpAKZAVlwdVUQCZB97odWfxXsj9mjtw2WFQcICeqaPJ6kDC6rvaaXZAv2LnRdE7sPttGhxJDleAq1WUqQEbHV";
-exports.API_VERSION = process.env.VERSION || "v17.0";
-exports.PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID || "114541121694377";
+exports.API_VERSION = "v17.0";
+exports.PHONE_NUMBER_ID = "114541121694377";
 exports.LIST_TITLE_DEFAULT_LABEL = "Selecionar um(a)";
 
 // General Detail
-exports.port = process.env.port || 3000;
+exports.port = 3000;
 exports.FILES_URL = "https://webhookoda-production.up.railway.app"; //your app server url
 exports.LOG_LEVEL = "info";
 
