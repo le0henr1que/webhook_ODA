02ba20a6823aea11757e00258a273748c90add94
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-07-08 11:16:14 -0300

    fix: new version

--- config/Config.js
+++ config/Config.js
@@ -1,14 +1,12 @@
 // ODA Details
-exports.ODA_WEBHOOK_URL =
-  "https://oda-cfc6b03f098a48c7b7365a21e619b4fa-da10.data.digitalassistant.oci.oraclecloud.com/connectors/v2/listeners/webhook/channels/42fe2ae4-5b75-421f-bc39-b5ec8c2cfd35";
-exports.ODA_WEBHOOK_SECRET = "svMjNN9DoW0bc1kAuiBvJfx2nBJeiBhy";
+exports.ODA_WEBHOOK_URL = process.env.URL_WEBHOOK_CLIENT;
+exports.ODA_WEBHOOK_SECRET = process.env.SECRET_WEBHOOK_CLIENT;
 
 // WhatsApp Details
 exports.API_URL = "https://graph.facebook.com";
 exports.ENDPOINT_API = "messages";
-exports.VERIFY_TOKEN = "access-token";
-exports.ACCESS_TOKEN =
-  "EAADaO3ZCOVKoBO5xLGme3Qkey8kyecWYqOaWTjynDTk4mcYhcOL9uxGWg1VCZB9gxhYqtSTR08alOd3RREBq16PWJMcTsHOlHCCpV7NAA9rqqZCetjAoRsyqMBbZA9t7ZBLc3CsFK38fpAKZAVlwdVUQCZB97odWfxXsj9mjtw2WFQcICeqaPJ6kDC6rvaaXZAv2LnRdE7sPttGhxJDleAq1WUqQEbHV";
+exports.VERIFY_TOKEN = process.env.MY_TOKEN;
+exports.ACCESS_TOKEN = process.env.WHATSAPP_TOKEN;
 exports.API_VERSION = "v17.0";
 exports.PHONE_NUMBER_ID = "114541121694377";
 exports.LIST_TITLE_DEFAULT_LABEL = "Selecionar um(a)";
